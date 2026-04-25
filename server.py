19473152414c37d841f7fa40585286fecf21a069
Chellah-nawel <chellah.nawel@gmail.com>
2026-04-25 22:26:39 +0100

    verifier certificat du clien et envoyer certificat du serveur

--- server.py
+++ server.py
@@ -5,3 +5,12 @@ server=socket.socket()
 server.bind(('localhost', 12345))#####################comment savoir le port d'ecoute?#####################
 server.listen(1) #le serveur attend une connexion
 adress, connexion =server.accept()
+
+#envoyer la certificat du serveur au client
+with open("server.crt", "rb") as f:
+    connexion.send(f.read())
+
+# La personne 3 te fournira une fonction comme :
+# verified = verify_certificate(cert_client)
+# if not verified:
+#     connexion.close()  # certificat invalide, on coupe
