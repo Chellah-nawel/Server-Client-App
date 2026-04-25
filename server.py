98e80f29765aadc47be19b3f3371df9c39c50bd7
Chellah-nawel <chellah.nawel@gmail.com>
2026-04-25 22:21:54 +0100

    recevoir la certificat du client

--- server.py
+++ server.py
@@ -4,4 +4,7 @@ import socket
 server=socket.socket()
 server.bind(('localhost', 12345))#####################comment savoir le port d'ecoute?#####################
 server.listen(1) #le serveur attend une connexion
-adress, connexion =server.accept()
+adress, connexion =server.accept() ###########################comment savoir l'adresse du client?#####################
+
+#recevoir la certificat du client
+client_certificate = connexion.recv(1024) ####################ca signifie quoi le 1024?#####################
